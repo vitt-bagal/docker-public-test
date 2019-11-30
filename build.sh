269b11e2b44dcc05926a4a952ac44a57009e4692
vitt-bagal <31851690+vitt-bagal@users.noreply.github.com>
2019-11-30 12:13:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script to add dep and user in image
 apt-get update -y
-useradd test -ms /bin/bash
-echo -e "test\ntest" | passwd test # changing password of test user to test
+useradd testuser -ms /bin/bash
+echo -e "test\ntest" | passwd testuser # changing password of test user to test
 apt-get install -y wget sudo 
